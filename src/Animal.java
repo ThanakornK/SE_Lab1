f411e383c2ac82123d6fda9247e44c585f68877d
Pond <thanakorn.kit@ku.th>
2020-12-16 11:08:31 +0700

    Update main and add Animal

---
+++ src/Animal.java
@@ -0,0 +1,18 @@
+public class Animal {
+
+    private String name;
+    private String sound;
+
+    public Animal(String name, String sound) {
+        this.name = name;
+        this.sound = sound;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+}
---
+++ src/MyClass.java
@@ -0,0 +1,7 @@
+public class MyClass {
+    public static void main(String[] args) {
+        Animal Lion = new Animal("Lion","Hong Hong");
+
+        System.out.println(Lion.getName() + " say " + Lion.getSound());
+    }
+}
